fe3add8e0cf7aaba3cf0d4d918046fd0ae6d019c
Vadim_P <vadimp_91@mail.ru>
2025-05-18 20:29:03 +0300

    Add api/get_products_list

--- product/web/api/api.py
+++ product/web/api/api.py
@@ -4,7 +4,7 @@ from product.product_repository.product_repository import ProductRepository
 from product.product_repository.unit_fo_work import UnitOfWork
 from product.product_service.product_service import ProductService
 from product.web.main import app
-from product.web.api.schemas import CreateProductSchema, GetProductSchema
+from product.web.api.schemas import CreateProductSchema, GetProductSchema, ProductResponse
 
 
 @app.post(
@@ -22,3 +22,16 @@ def create_product(payload: CreateProductSchema):
         return_payload = product.dict()
 
     return return_payload
+
+
+@app.get(
+    "/products",
+    status_code=status.HTTP_200_OK,
+    response_model=ProductResponse,
+)
+def get_products_list(limit: int = None):
+    with UnitOfWork() as unit_of_work:
+        repo = ProductRepository(unit_of_work.session)
+        product_service = ProductService(repo)
+        all_products = product_service.list_products(limit=limit)
+        return {"products": [product.dict() for product in all_products]}
