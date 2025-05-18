e48fd9989d8e30de9a7717b221b9546de6a5486d
Vadim_P <vadimp_91@mail.ru>
2025-05-18 20:28:26 +0300

    fix

--- product/product_service/product_service.py
+++ product/product_service/product_service.py
@@ -20,7 +20,7 @@ class ProductService:
         product = self.product_repository.get(product_name)
         if product is None:
             raise ProductNotFoundError(f"Product with id {product_name} is not found")
-        self.product_repository.update(product_name, new_product)
+        return self.product_repository.update(product_name, new_product)
 
     def delete_product(self, product_name):
         product = self.product_repository.get(product_name)
@@ -30,4 +30,4 @@ class ProductService:
 
     def list_products(self, **filters):
         limit = filters.pop("limit", None)
-        self.product_repository.get_list(limit, **filters)
+        return self.product_repository.get_list(limit, **filters)
