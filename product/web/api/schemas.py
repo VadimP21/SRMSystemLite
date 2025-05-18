f7e4afe1316893b91374bbbe8ed4fffef468dc5f
Vadim_P <vadimp_91@mail.ru>
2025-05-18 21:52:29 +0300

    Add SortField, SortOrder to api/schemas

--- product/web/api/schemas.py
+++ product/web/api/schemas.py
@@ -1,8 +1,21 @@
 from datetime import datetime
 from decimal import Decimal
+from enum import Enum
 from typing import List
 
 from pydantic import BaseModel, ConfigDict
+
+
+class SortField(str, Enum):
+    name = "name"
+    price = "price"
+    created_at = "created_at"
+
+
+# Определите перечисление для направления сортировки
+class SortOrder(str, Enum):
+    asc = "asc"
+    desc = "desc"
 
 
 class CreateProductSchema(BaseModel):
@@ -13,6 +26,7 @@ class CreateProductSchema(BaseModel):
 class GetProductSchema(CreateProductSchema):
     id: int
     created_at: datetime
+
 
 class ProductResponse(BaseModel):
     products: List[GetProductSchema]
