e916f92a5dd378602267dfa1d922579b05228197
Vadim_P <vadimp_91@mail.ru>
2025-05-20 16:18:53 +0300

    Add api/get put delete

--- product/web/api/api.py
+++ product/web/api/api.py
@@ -1,8 +1,9 @@
-from fastapi import Query
+from fastapi import Query, HTTPException
 from starlette import status
 
 from product.product_repository.product_repository import ProductRepository
 from product.product_repository.unit_fo_work import UnitOfWork
+from product.product_service.exeptions import ProductNotFoundError
 from product.product_service.product_service import ProductService
 from product.web.main import app
 from product.web.api.schemas import (
@@ -32,7 +33,6 @@ def create_product(payload: CreateProductSchema):
 
 @app.get(
     "/products",
-    status_code=status.HTTP_200_OK,
     response_model=ProductResponse,
 )
 def get_products_list(
@@ -48,4 +48,64 @@ def get_products_list(
         all_products = product_service.list_products(
             limit=limit, offset=offset, sort_field=sort_field, sort_order=sort_order
         )
-        return {"products": [product.dict() for product in all_products]}
+    return {"products": [product.dict() for product in all_products]}
+
+
+@app.get(
+    "/product/{product_name}",
+    response_model=GetProductSchema,
+)
+def get_product(product_name: str):
+    try:
+        with UnitOfWork() as unit_of_work:
+            repo = ProductRepository(unit_of_work.session)
+            product_service = ProductService(repo)
+
+            result = product_service.get_product(product_name=product_name)
+
+        return result.dict()
+    except ProductNotFoundError:
+        raise HTTPException(
+            status_code=404, detail=f"Product '{product_name}' not found"
+        )
+
+
+@app.put(
+    "/product/{product_name}",
+    response_model=GetProductSchema,
+)
+def update_product(product_name: str, product_details: CreateProductSchema):
+    try:
+        with UnitOfWork() as unit_of_work:
+            repo = ProductRepository(unit_of_work.session)
+            product_service = ProductService(repo)
+            new_product = product_details.model_dump()
+
+            result = product_service.update_product(
+                product_name=product_name, new_product=new_product
+            )
+            unit_of_work.commit()
+        return result.dict()
+    except ProductNotFoundError:
+        raise HTTPException(
+            status_code=404, detail=f"Product '{product_name}' not found"
+        )
+
+
+@app.delete(
+    "/product/{product_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+def delete_product(product_id: int):
+    try:
+        with UnitOfWork() as unit_of_work:
+            repo = ProductRepository(unit_of_work.session)
+            product_service = ProductService(repo)
+            product_service.delete_product(product_id=product_id)
+
+            unit_of_work.commit()
+        return
+    except ProductNotFoundError:
+        raise HTTPException(
+            status_code=404, detail=f"Product with ID '{product_id}' not found"
+        )
