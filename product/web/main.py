457e0bb11b028eadb625c11dce019e01d407e1b1
Vadim_P <vadimp_91@mail.ru>
2025-05-21 19:43:59 +0300

    Add oas to main

--- product/web/main.py
+++ product/web/main.py
@@ -1,8 +1,15 @@
+from pathlib import Path
+
+import yaml
 from fastapi import FastAPI
 
 
 app = FastAPI(
-    debug=True,
+    debug=True, openapi_url="/openapi/products.json", docs_url="/docs/product"
 )
 
+
+oas_doc = yaml.safe_load((Path(__file__).parent / "../openapi.yaml").read_text())
+
+app.openapi = lambda: oas_doc
 from product.web.api import api
