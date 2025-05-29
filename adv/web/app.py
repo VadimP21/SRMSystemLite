3c5726e101743a261ff6213c27a346edf6216e2c
Vadim_P <vadimp_91@mail.ru>
2025-05-29 14:30:29 +0300

    Add adv_flask_app

---
+++ adv/web/app.py
@@ -0,0 +1,16 @@
+"""
+Инициализация объекта приложения Flask и объекта Api
+"""
+from flask import Flask
+from flask_smorest import Api
+
+from adv.web.api.api import blueprint
+from adv.web.config import BaseConfig
+
+app = Flask(__name__)
+app.config.from_object(BaseConfig)
+
+adv_api = Api
+
+adv_api.register_blueprint(blueprint)
+
